c047f0233a619a14282ac954f254e191ef20756a
Charlie <CharlieLSparrow@gmail.com>
2026-01-24 14:38:47 -0700

    Photo Album first try

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,4 @@
-// Requirement: Dynamically populate the current year
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
-// Requirement: Dynamically populate the last modified date
 document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('open');
+});
